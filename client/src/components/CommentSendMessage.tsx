2e2ed78429110f2f5e33de7338bfe19e9e3211b2
Vladimir Aleksic <vlada.pzs@hotmail.com>
2023-02-10 13:49:47 +0100

    Input looks

--- client/src/components/CommentSendMessage.tsx
+++ client/src/components/CommentSendMessage.tsx
@@ -1,5 +1,21 @@
+import { FiArrowRight } from 'react-icons/fi'
+
 const CommentSendMessage = () => {
-  return <div>CommentSendMessage</div>
+  return (
+    <form
+      action=''
+      className='bg-gray-100 w-full h-20 flex justify-between'
+    >
+      <input
+        type='text'
+        placeholder='Type a comment...'
+        className='w-full h-full p-4 outline-none'
+      />
+      <button className='w-20 bg-cyan-300 shadow-cyan-200 shadow-lg flex justify-center items-center'>
+        <FiArrowRight className='w-6 h-6' />
+      </button>
+    </form>
+  )
 }
 
 export default CommentSendMessage
--- client/src/components/CommentsBox.tsx
+++ client/src/components/CommentsBox.tsx
@@ -6,7 +6,30 @@ const CommentsBox = () => {
   return (
     <div className='h-[600px] w-96 bg-gradient-to-b from-gray-100 to-gray-200 rounded-t-3xl flex flex-col justify-between items-center shadow-xl'>
       <CommentsHeader />
-      <Comment />
+      <div className='w-full h-[500px] overflow-y-scroll'>
+        <Comment />
+        <Comment />
+        <Comment />
+        <Comment />
+        <Comment />
+        <Comment />
+        <Comment />
+        <Comment />
+        <Comment />
+        <Comment />
+        <Comment />
+        <Comment />
+        <Comment />
+        <Comment />
+        <Comment />
+        <Comment />
+        <Comment />
+        <Comment />
+        <Comment />
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
       <CommentSendMessage />
     </div>
   )
